b9c084c6b9835caf94654842d5cfca2c4c4b87a1
Bryan Blaze <crash.man000@gmail.com>
2025-02-13 14:14:57 -0700

    Added Gameover Scene with Retry - Level
    - Created Scene Gameover
    - Added Gameover scen to Unity Build Settings
    - Updated IntroScene to fix Elevator
    - Changed the PlayerDeadState.cs to load Gameover scene
    - Updated AudioManager.cs to be able to Stop Music/SFX, updated public method summaries and renamed variable for easy to read.
    - Updated GameManager.cs public method summaries, added methods to be able to track previous scene and methods to exchange scene name to build index or opposite, and to play correct music on restart level
    - Updated GAMEOVER.cs to add the death SFX and to stop music on death.
    - Updated UISpriteAnimation.cs public summary
    - Updated Elevator prefab to not update parent on load
    - Updated AudioManager prefab to start music at half sound volumn because music is louder than SFX, and needs to be turned down.

--- Assets/_GearedFate/Code/Scripts/Manager/AudioManager.cs
+++ Assets/_GearedFate/Code/Scripts/Manager/AudioManager.cs
@@ -14,27 +14,58 @@ namespace BTG
         private AudioSource audioSource;
 
         [SerializeField]
-        private AudioClip[] clips;
+        private AudioClip[] musicClips;
 
         [SerializeField]
         private AudioSource _sfxSource;
 
+        /// <summary>
+        /// Play an SFX audio clip.
+        /// </summary>
+        /// <param name="clip"> The AudioClip to play. </param>
         public void PlaySFX(AudioClip clip)
         {
-            _sfxSource.clip = clip; /// 3 -> 1, 5 -> 2 etc.
+            _sfxSource.clip = clip;
             _sfxSource.Play();
         }
 
-        public void PlayCorrrectClip(int index)
+        /// <summary>
+        /// Plays the corresponding music clip based on the given index.
+        /// The index is expected to be an odd number (1, 3, 5, etc.).
+        /// The actual music clip index is calculated as (index - 1) / 2.
+        /// e.g. 3 -> 1, 5 -> 2 etc.
+        /// </summary>
+        /// <param name="index">The index of the music clip to play.</param>
+        public void PlayCorrrectMusicClip(int index)
         {
-            audioSource.clip = clips[(index - 1) / 2]; /// 3 -> 1, 5 -> 2 etc.
+            audioSource.clip = musicClips[(index - 1) / 2];
             audioSource.Play();
         }
 
-        public void PlayMenuClip()
+        /// <summary>
+        /// Plays the first music clip in the `musicClips` array.
+        /// This is typically used to play the background music for the main menu.
+        /// </summary>
+        public void PlayMenuMusicClip()
         {
-            audioSource.clip = clips[0];
+            audioSource.clip = musicClips[0];
             audioSource.Play();
+        }
+
+        /// <summary>
+        /// Stops all music playback.
+        /// </summary>
+        public void StopMusic()
+        {
+            audioSource.Stop();
+        }
+
+        /// <summary>
+        /// Stops all SFX playback.
+        /// </summary>
+        public void StopSFX()
+        {
+            _sfxSource.Stop();
         }
     }
 }
--- Assets/_GearedFate/Code/Scripts/Manager/GameManager.cs
+++ Assets/_GearedFate/Code/Scripts/Manager/GameManager.cs
@@ -2,6 +2,8 @@
 
 namespace BTG
 {
+    using System.IO;
+    using System.Linq;
     using UnityEngine;
     using UnityEngine.SceneManagement;
     using UnityUtils;
@@ -14,59 +16,129 @@ namespace BTG
         // ... other game manager code ...
         [Header("Scene Settings")]
         [SerializeField]
-        private string mainMenuScene = "MainMenu";
+        private string _mainMenuScene = "MainMenu";
 
         [SerializeField]
-        private string gameOver = "GameOver";
+        private string _gameOver = "Gameover";
 
         [SerializeField]
-        private string credits = "Credits";
+        private string _credits = "Credits";
 
-        // [SerializeField] string introScene = "IntroScene";
         [SerializeField]
-        private string[] bossLevelScenes =
+        private string[] _bossLevelScenes =
         {
             "BossLevel 1", "BossLevel 2", "BossLevel 3",
             "BossLevel 4", "BossLevel 5",
         };
 
         [SerializeField]
-        private string[] cutsceneScenes =
+        private string[] _cutsceneScenes =
         {
             "Cutscene 0", "Cutscene 1", "Cutscene 2",
             "Cutscene 3", "Cutscene 4", "Cutscene 5",
         };
 
-        private string currentScene;
+        private string _currentScene;
+        private string _previousScene;
 
-        public void LoadMainMenu()
+        /// <summary>
+        /// Gets the build index of a scene given its name.
+        /// </summary>
+        /// <param name="sceneName">The name of the scene.</param>
+        /// <returns>The build index of the scene, or -1 if the scene is not found.</returns>
+        public static int GetBuildIndexFromSceneName(string sceneName)
         {
-            AudioManager.Instance.PlayMenuClip();
-            LoadScene(mainMenuScene);
+            for (int i = 0; i < SceneManager.sceneCountInBuildSettings; i++)
+            {
+                string scenePath = SceneUtility.GetScenePathByBuildIndex(i);
+                string sceneFileName = Path.GetFileNameWithoutExtension(scenePath);
+
+                if (sceneFileName == sceneName)
+                {
+                    return i;
+                }
+            }
+
+            return -1; // Scene not found
         }
 
+        /// <summary>
+        /// Gets the name of a scene given its build index.
+        /// </summary>
+        /// <param name="buildIndex">The build index of the scene.</param>
+        /// <returns>The name of the scene, or an empty string if the index is invalid.</returns>
+        public static string GetSceneNameFromBuildIndex(int buildIndex)
+        {
+            if (buildIndex >= 0 && buildIndex < SceneManager.sceneCountInBuildSettings)
+            {
+                return SceneUtility.GetScenePathByBuildIndex(buildIndex).Split('/').Last().Split('.')[0];
+            }
+            else
+            {
+                return string.Empty; // Return an empty string for invalid indices
+            }
+        }
+
+        /// <summary>
+        /// Loads the Main Menu.
+        /// </summary>
+        public void LoadMainMenu()
+        {
+            LoadScene(_mainMenuScene);
+            AudioManager.Instance.PlayMenuMusicClip();
+        }
+
+        /// <summary>
+        /// Starts the game on Cutscene 0.
+        /// </summary>
         public void StartGame()
         {
             LoadScene("Cutscene 0");
         }
 
+        /// <summary>
+        /// Gets the string of the current scene.
+        /// </summary>
+        /// <returns> Current Scene string. </returns>
         public string GetCurrentScene()
         {
-            return currentScene;
+            return _currentScene;
         }
 
+        /// <summary>
+        /// Gets the string of the previous scene.
+        /// </summary>
+        /// <returns> Previous Scene string. </returns>
+        public string GetPreviousScene()
+        {
+            return _previousScene;
+        }
+
+        /// <summary>
+        /// Gets the strings of all Cutscenes.
+        /// </summary>
+        /// <returns> Array of Cutscene strings. </returns>
         public string[] GetCutsceneScenes()
         {
-            return cutsceneScenes;
+            return _cutsceneScenes;
         }
 
+        /// <summary>
+        /// Gets the strings of all Boss Level scenes.
+        /// </summary>
+        /// <returns> Array of Boss Level strings. </returns>
         public string[] GetBossLevelScenes()
         {
-            return bossLevelScenes;
+            return _bossLevelScenes;
         }
 
+        /// <summary>
+        /// Loads the next level in the Build index.
+        /// </summary>
         public void LoadNextLevel()
         {
+            _previousScene = GetCurrentScene();
+
             var currentBuildScene = SceneManager.GetActiveScene().buildIndex;
             if (currentBuildScene >= 10)
             {
@@ -77,26 +149,59 @@ namespace BTG
                 SceneManager.LoadScene(++currentBuildScene);
                 if (currentBuildScene % 2 != 0)
                 {
-                    AudioManager.Instance.PlayCorrrectClip(currentBuildScene);
+                    AudioManager.Instance.PlayCorrrectMusicClip(currentBuildScene);
                 }
+            }
+
+            _currentScene = GetSceneNameFromBuildIndex(currentBuildScene);
+
+#if UNITY_EDITOR
+            Debug.Log("From LoadNextLevel:");
+            Debug.Log("Current Scene = " + _currentScene);
+            Debug.Log("Previous Scene = " + _previousScene);
+#endif
+        }
+
+        /// <summary>
+        /// Loads the previous scene from the Gameover scene or restarts current scene.
+        /// </summary>
+        public void RestartLevel()
+        {
+            if (GetCurrentScene().Equals("Gameover"))
+            {
+                int previousSceneIndex = GetBuildIndexFromSceneName(_previousScene);
+                int musicIndex = previousSceneIndex % 2 == 0 ? previousSceneIndex : previousSceneIndex - 1;
+
+                AudioManager.Instance.PlayCorrrectMusicClip(musicIndex);
+                LoadScene(_previousScene);
+
+                Debug.Log("MusicSceneIndex = " + musicIndex);
+            }
+            else
+            {
+                LoadScene(_currentScene);
             }
         }
 
-        public void RestartLevel()
-        {
-            LoadScene(currentScene);
-        }
-
+        /// <summary>
+        /// Loads the game over scene.
+        /// </summary>
         public void LoadGameOver()
         {
-            LoadScene(gameOver);
+            LoadScene(_gameOver);
         }
 
+        /// <summary>
+        /// Loads the credits scene.
+        /// </summary>
         public void LoadCredits()
         {
-            LoadScene(credits);
+            LoadScene(_credits);
         }
 
+        /// <summary>
+        /// Quits the game.
+        /// </summary>
         public void QuitGame()
         {
             Application.Quit();
@@ -106,13 +211,20 @@ namespace BTG
         {
             base.Awake(); // Important: Call the base Singleton Awake!
 
-            currentScene = SceneManager.GetActiveScene().name;
+            _currentScene = SceneManager.GetActiveScene().name;
         }
 
         private void LoadScene(string sceneName)
         {
-            currentScene = sceneName;
+            _previousScene = GetCurrentScene();
+            _currentScene = sceneName;
             SceneManager.LoadScene(sceneName);
+
+#if UNITY_EDITOR
+            Debug.Log("From LoadScene:");
+            Debug.Log("Current Scene = " + _currentScene);
+            Debug.Log("Previous Scene = " + _previousScene);
+#endif
         }
     }
 }
